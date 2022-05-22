75dad8f88515f27ce787c14964c5051f9cbb6a43
Ndejjeco-ICT <club.ict@outlook.com>
2022-05-22 08:47:09 -0700

    Glance updated

--- src/ns/components/views/About/Glance/glance.ts
+++ src/ns/components/views/About/Glance/glance.ts
@@ -4,29 +4,46 @@ const Template_ = document.createElement("template");
 Template_.innerHTML = `
 <div class="xb-glance-component">
     <div class="xb-wrapper">
-        <div class="tr-title-area-wrapper">
-            <div class="tr-title-container">Ndejje At A Glance</div>
-        </div>
         <div class="tr-split-view">
             <div class="tr-view-1">
                 <div class="tr-view-1-wrapper">
                     <div class="tr-data-cpr">
-                        <div class="cl-title">Boys</div>
-                        <div class="cl-figure">100</div>
+                        <div class="cpr-wrapper">
+                            <div class="cl-title">
+                                <div class="cl-title-wrapper">1963</div>
+                            </div>
+                            <div class="cl-figure">
+                                <div class="cl-figure-wrapper">The Year Ndejje was Established</div>
+                            </div>
+                        </div>
                     </div>
                     <div class="tr-data-cpr">
-                        <div class="cl-title">Girls</div>
-                        <div class="cl-figure">100</div>
+                        <div class="cpr-wrapper">
+                            <div class="cl-title">
+                                <div class="cl-title-wrapper">162</div>
+                            </div>
+                            <div class="cl-figure">
+                                <div class="cl-figure-wrapper">A Large Number of Staff Members</div>
+                            </div>
+                        </div>
                     </div>
                     <div class="tr-data-cpr">
-                        <div class="cl-title">Staff Members</div>
-                        <div class="cl-figure">100</div>
+                        <div class="cpr-wrapper">
+                            <div class="cl-title">
+                                <div class="cl-title-wrapper">2100</div>
+                            </div>
+                            <div class="cl-figure">
+                                <div class="cl-figure-wrapper">With A Vast Number of Students</div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
             <div class="tr-view-2">
                 <div class="tr-view-2-wrapper">
-                    <div class="cl-picture-container"></div>
+                    <div class="cl-title-area">Ndejje At A Glance</div>
+                    <div class="cl-picture-container">__image__</div>
                 </div>
             </div>
         </div>
